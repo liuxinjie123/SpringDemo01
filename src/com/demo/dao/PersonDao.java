818aae5d89249c95b7046007dead1589e887af03
1134005157 <1134005157@qq.com>
2021-01-12 13:29:08 +0800

    spring demo 01

---
+++ src/com/demo/dao/PersonDao.java
@@ -0,0 +1,5 @@
+package com.demo.dao;
+
+public interface PersonDao {
+    public void add();
+}
---
+++ src/com/demo/dao/PersonDaoImpl.java
@@ -0,0 +1,8 @@
+package com.demo.dao;
+
+public class PersonDaoImpl implements PersonDao {
+    @Override
+    public void add() {
+        System.out.println(" executed add() method ... ");
+    }
+}
